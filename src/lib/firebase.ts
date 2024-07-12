de1db0909cb454da3fb3a2869f43ec7bc75f7126
Kacper <kacper.gajko1@gmail.com>
2024-07-12 09:07:20 +0200

    config for firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,15 @@
+import { env } from '$env/dynamic/private';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID
+	apiKey: env.FIREBASE_API_KEY,
+	authDomain: env.FIREBASE_AUTH_DOMAIN,
+	projectId: env.FIREBASE_PROJECT_ID,
+	storageBucket: env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: env.FIREBASE_APP_ID,
+	measurementId: env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
