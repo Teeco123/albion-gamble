d34e13a557b9c64f86141adb32086709c2690d17
Kacper <kacper.gajko1@gmail.com>
2024-07-12 23:10:27 +0200

    working login

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,45 @@
+import { firestore } from '$lib/firebase/firebase.js';
+import { query, collection, where, getDocs, setDoc, doc } from 'firebase/firestore';
+
+export const actions = {
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const username = data.get('username');
+		const password = data.get('password');
+		const server = data.get('server');
+
+		if (!username || !password || !server) {
+			return { missing: true };
+		}
+
+		let userData;
+		let userId = '';
+		const userQuery = query(
+			collection(firestore, 'users'),
+			where('username', '==', username),
+			where('server', '==', server),
+			where('password', '==', password)
+		);
+		const userSnapshot = await getDocs(userQuery);
+		userSnapshot.forEach((userDoc) => {
+			userData = userDoc.data();
+			userId = userDoc.id;
+		});
+
+		if (!userData) {
+			return { wrong: true };
+		}
+
+		if (userData) {
+			const sessionId = crypto.randomUUID();
+
+			setDoc(doc(firestore, 'users', userId), { sessionId: sessionId }, { merge: true });
+			cookies.set('sessionId', sessionId, {
+				path: '/',
+				httpOnly: true,
+				secure: true,
+				maxAge: 60 * 60 * 24 * 1
+			});
+		}
+	}
+};
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let form;
+</script>
+
+<form method="POST" action="?/login">
+	{#if form?.missing}<p class="error">Fill required forms</p>{/if}
+	{#if form?.wrong}<p class="error">Wrong username or password</p>{/if}
+	<label>
+		Username
+		<input name="username" type="string" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" />
+	</label>
+	<label>
+		Server
+		<select name="server">
+			<option value="Europe">Europe</option>
+			<option value="America">America</option>
+			<option value="Asia">Asia</option>
+		</select>
+	</label>
+	<button type="submit">Register</button>
+</form>
+
+<style lang="scss">
+</style>
