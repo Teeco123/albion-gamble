ed92e5bc0f827f248979770493b49491fd022502
Kacper <kacper.gajko1@gmail.com>
2024-07-12 08:59:17 +0200

    register form

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,18 @@
+<form method="POST" action="?/register">
+	<label>
+		Username
+		<input name="username" type="string" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" />
+	</label>
+	<label>
+		Server
+		<select name="server">
+			<option value="Europe">Europe</option>
+			<option value="America">America</option>
+			<option value="Asia">Asia</option>
+		</select>
+	</label>
+</form>
