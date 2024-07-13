0fb0bc70dcef6c7772d885e49086d3af36d247a4
Kacper <kacper.gajko1@gmail.com>
2024-07-13 08:15:19 +0200

    config for sveltefire

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/private';
+	import { initializeApp } from 'firebase/app';
+	import { getFirestore } from 'firebase/firestore';
+	import { FirebaseApp } from 'sveltefire';
+
+	const firebaseConfig = {
+		apiKey: env.FIREBASE_API_KEY,
+		authDomain: env.FIREBASE_AUTH_DOMAIN,
+		projectId: env.FIREBASE_PROJECT_ID,
+		storageBucket: env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+		appId: env.FIREBASE_APP_ID,
+		measurementId: env.FIREBASE_MEASUREMENT_ID
+	};
+
+	const app = initializeApp(firebaseConfig);
+	const firestore = getFirestore(app);
+</script>
+
+<FirebaseApp {firestore}>
+	<slot />
+</FirebaseApp>
