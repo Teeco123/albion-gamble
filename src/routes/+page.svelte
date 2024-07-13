76f7614680ae1195ae7cee2328cbd5f98ccccd24
Kacper <kacper.gajko1@gmail.com>
2024-07-13 11:33:27 +0200

    using nested components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,11 @@
 <script lang="ts">
-	import { firestore } from '$lib/firebase';
-	import { pusherClient } from '$lib/pusher/client';
-	import { docStore } from 'sveltefire';
 	import type { PageData } from './$types';
+	import Header from './_components/header.svelte';
 
 	export let data: PageData;
-
-	interface User {
-		username?: string;
-		password?: string;
-		server?: string;
-		balance?: number;
-	}
-	const user = docStore<User>(firestore, `users/${data.userId}`);
-
-	const channel = pusherClient.subscribe('channel');
-	channel.bind('event', function (data: any) {
-		console.log(data);
-	});
 </script>
 
-<div>{$user?.balance}</div>
+<Header {data} />
 
 <style lang="scss">
 </style>
---
+++ src/routes/_components/header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { firestore } from '$lib/firebase';
+	import { docStore } from 'sveltefire';
+
+	export let data;
+
+	interface User {
+		username?: string;
+		password?: string;
+		server?: string;
+		balance?: number;
+	}
+
+	const user = docStore<User>(firestore, `users/${data.userId}`);
+</script>
+
+<div>{$user?.balance}</div>
+
+<style lang="scss">
+</style>
