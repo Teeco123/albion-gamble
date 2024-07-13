fd30bda6a4428932756e762f145be2271dc764d7
Kacper <kacper.gajko1@gmail.com>
2024-07-13 09:02:05 +0200

    changed how env imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import { env } from '$env/dynamic/private';
 	import { initializeApp } from 'firebase/app';
 	import { getFirestore } from 'firebase/firestore';
 	import { FirebaseApp } from 'sveltefire';
 
 	const firebaseConfig = {
-		apiKey: env.FIREBASE_API_KEY,
-		authDomain: env.FIREBASE_AUTH_DOMAIN,
-		projectId: env.FIREBASE_PROJECT_ID,
-		storageBucket: env.FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-		appId: env.FIREBASE_APP_ID,
-		measurementId: env.FIREBASE_MEASUREMENT_ID
+		apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+		authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+		storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+		appId: import.meta.env.VITE_FIREBASE_APP_ID,
+		measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 	};
 
 	const app = initializeApp(firebaseConfig);
