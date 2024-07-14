c5e46ed9b7b6c199bd97aba009345ff3d942c15d
Kacper <kacper.gajko1@gmail.com>
2024-07-14 02:49:13 +0200

    add message to db

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,13 @@
-import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import {
+	collection,
+	doc,
+	getDocs,
+	query,
+	updateDoc,
+	where,
+	addDoc,
+	serverTimestamp
+} from 'firebase/firestore';
 import { firestore } from '$lib/firebase';
 import { redirect } from '@sveltejs/kit';
 
@@ -53,5 +62,32 @@ export const actions = {
 	},
 	registerRedirect: async () => {
 		redirect(302, '/register');
+	},
+	sendMessage: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const message = data.get('message');
+
+		let sessionId = cookies.get('sessionId');
+
+		if (sessionId == undefined) {
+			sessionId = '';
+		}
+
+		let userId;
+		const userDataQuery = query(
+			collection(firestore, 'users'),
+			where('sessionId', '==', sessionId)
+		);
+
+		const userSnapshot = await getDocs(userDataQuery);
+		userSnapshot.forEach((doc) => {
+			userId = doc.id;
+		});
+
+		await addDoc(collection(firestore, 'messages'), {
+			userId: userId,
+			message: message,
+			timeSent: serverTimestamp()
+		});
 	}
 };
