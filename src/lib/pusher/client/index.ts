49dd4394b7640fc5ef85cbc18b29283f56d4427d
Kacper <kacper.gajko1@gmail.com>
2024-07-18 15:01:11 +0200

    pusher cfg

--- src/lib/pusher/client/index.ts
+++ src/lib/pusher/client/index.ts
@@ -1,5 +1,10 @@
 import PusherClient from 'pusher-js';
 
+//@ts-ignore
 export const pusherClient = new PusherClient('99c59095535b5193797a', {
-	cluster: 'eu'
+	wsHost: '127.0.0.1',
+	wssPort: 6001,
+	forceTLS: true,
+	disableStats: true,
+	enabledTransports: ['wss']
 });
--- src/lib/pusher/server/index.ts
+++ src/lib/pusher/server/index.ts
@@ -1,10 +1,12 @@
 import PusherServer from 'pusher';
 import { env } from '$env/dynamic/private';
 
+//@ts-ignore
 export const pusherServer = new PusherServer({
-	appId: env.PUSHER_APP_ID as string,
-	key: '99c59095535b5193797a',
-	secret: env.PUSHER_SECRET as string,
-	cluster: 'eu',
+	appId: env.SOKETI_DEFAULT_APP_ID as string,
+	key: env.SOKETI_DEFAULT_APP_KEY as string,
+	secret: env.SOKETI_DEFAULT_APP_SECRET as string,
+	host: '127.0.0.1',
+	port: 3002,
 	useTLS: true
 });
