74b7165aa9e7edc7c2a1d9d947678d6dabaca783
Kacper <kacper.gajko1@gmail.com>
2024-07-20 09:23:43 +0200

    working site with domain but with not secure tag

--- sveltekit/server.js
+++ sveltekit/server.js
@@ -1,13 +1,13 @@
 import { handler } from './build/handler.js';
 import express from 'express';
 import * as fs from 'fs';
-import * as http from 'http';
+import * as https from 'https';
 
 const app = express();
 
 app.use(handler);
 
-http.createServer(app).listen(3000, () => {
+https.createServer(app).listen(3000, () => {
 	console.log('https server is runing at port 3000');
 });
 
