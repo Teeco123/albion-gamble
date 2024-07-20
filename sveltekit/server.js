bf6e11da27b6b3e059f670afb46ae80275ac30e2
Kacper <kacper.gajko1@gmail.com>
2024-07-20 11:40:00 +0200

    working site with https

--- sveltekit/server.js
+++ sveltekit/server.js
@@ -1,13 +1,12 @@
 import { handler } from './build/handler.js';
 import express from 'express';
-import * as fs from 'fs';
-import * as https from 'https';
+import * as http from 'http';
 
 const app = express();
 
 app.use(handler);
 
-https.createServer(app).listen(3000, () => {
+http.createServer(app).listen(3000, () => {
 	console.log('https server is runing at port 3000');
 });
 
